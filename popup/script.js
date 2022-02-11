a5a7acee5cb7234004db724d4d25cadcf27eac65
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2022-02-11 17:59:25 +0100

    style changes

--- popup/script.js
+++ popup/script.js
@@ -1,6 +1,6 @@
 let storage = browser.storage.sync.get().then(r=>{
 	document.getElementById("succeeded").innerText = r.succeeded
-	document.getElementById("failed").innerText = `with ${r.failed} failed attempts`
+	document.getElementById("failed").innerText = `with ${r.failed} failed logins`
 
 })
 
