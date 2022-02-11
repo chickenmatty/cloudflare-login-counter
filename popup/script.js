bae7a420338f174e619320a1ebb7f371432731a6
ssebastianoo <sebastianogirotto05@gmail.com>
2022-02-11 19:08:55 +0100

    new frontend

--- popup/script.js
+++ popup/script.js
@@ -1,6 +1,6 @@
 let storage = browser.storage.sync.get().then(r=>{
-	document.getElementById("succeeded").innerText = r.succeeded
-	document.getElementById("failed").innerText = `with ${r.failed} failed logins`
+	document.getElementById("succeeded").innerText = r.succeeded;
+	document.getElementById("failed").innerText = r.failed;
 
 })
 
