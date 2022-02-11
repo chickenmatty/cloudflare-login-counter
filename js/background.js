4b34d14aa0f67c0d13e58d5a5400e7599f040fda
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2022-02-11 15:34:52 +0100

    rename success and fail

--- js/background.js
+++ js/background.js
@@ -1,8 +1,8 @@
 // init sync storage
 async function initStorage() {
 	let storage = await browser.storage.sync.get()
-	if(storage.success === undefined && storage.fail == undefined){
-		await browser.storage.sync.set({success:0, fail:0})
+	if(storage.succeeded === undefined && storage.failed == undefined){
+		await browser.storage.sync.set({succeeded:0, failed:0})
 	}
 }
 
@@ -12,8 +12,8 @@ browser.webRequest.onCompleted.addListener(async (details) => {
 	let storage = await browser.storage.sync.get()
 
 	if(details.statusCode===200){
-		await browser.storage.sync.set({success:storage.success+1})
+		await browser.storage.sync.set({succeeded:storage.succeeded+1})
 	}else{
-		await browser.storage.sync.set({fail:storage.fail+1})
+		await browser.storage.sync.set({failed:storage.failed+1})
 	}
 } ,{urls:["https://dash.cloudflare.com/api/v4/login"]})
--- popup/script.js
+++ popup/script.js
@@ -1,6 +1,6 @@
 let storage = browser.storage.sync.get().then(r=>{
-	document.getElementById("success-counter").innerText = r.success
-	document.getElementById("fail-counter").innerText = r.fail
+	document.getElementById("succeeded").innerText = r.succeeded
+	document.getElementById("failed").innerText = `with ${r.failed} failed attempts`
 
 })
 
